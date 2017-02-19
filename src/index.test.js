6950fd2d4f33b5cde716e0d7eb697d87c86ba2e5
Sebastian Grebe <sebigrebe@googlemail.com>
2017-02-19 13:00:59 +0100

    fix(build): Test Fix

--- src/index.test.js
+++ src/index.test.js
@@ -1,22 +1,20 @@
  var expect = require('chai').expect;
  var chartit = require('./index'); 
- var config={
-                grid:{
-                    padding:{
-                        left:5
-                    }
-                }
-            };
-var chart=chartit.init(config);
+ var config=
+ {
+    grid:{
+        padding:{
+            left:5
+        }
+    }
+};
+var chart=new chartit(config);
 
  describe('chartit', function(){
     it ('should work!',function(){
         expect(true).to.be.true;
     });
-    describe('init', function(){
-        var chart=chartit.init(config);
-        it ('should merge the config Objects',function(){
-            expect(chart.config).to.contain.all.keys({'grid':{'padding':['top','right','bottom','left']}});
-        });
+    it ('should merge the config Objects',function(){
+        expect(chart.config).to.contain.all.keys({'grid':{'padding':['top','right','bottom','left']}});
     });
  });
