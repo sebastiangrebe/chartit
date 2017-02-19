3a9852c7fa103d2d050b2879db12df04821efd49
Sebastian Grebe <sebigrebe@googlemail.com>
2017-02-19 12:56:56 +0100

    feat(build): Class

--- src/index.js
+++ src/index.js
@@ -1,10 +1,44 @@
+'use strict';
+
 var DEFAULT_CONFIG = { 
     grid: {
+        padding:{
+            top:0,
+            right:0,
+            bottom:0,
+            left:0
+        }
+    }
+};
 
+class chartit {
+
+    constructor(config){
+        var config=this.mergeDeep(DEFAULT_CONFIG, config);
+        this.config=config;
+    }
+
+    isObject(item) {
+        return (item && typeof item === 'object' && !Array.isArray(item) && item !== null);
+    }
+
+    mergeDeep(target, source) {
+        let output = Object.assign({}, target);
+        if (this.isObject(target) && this. isObject(source)) {
+            Object.keys(source).forEach(key => {
+            if (this.isObject(source[key])) {
+                if (!(key in target))
+                Object.assign(output, { [key]: source[key] });
+                else
+                output[key] = this.mergeDeep(target[key], source[key]);
+            } else {
+                Object.assign(output, { [key]: source[key] });
+            }
+            });
+        }
+        return output;
     }
 }
 
-module.exports={
-    init:function(){},
-    draw:function(){}
-};
+
+module.exports=chartit;
--- src/index.test.js
+++ src/index.test.js
@@ -1,8 +1,22 @@
  var expect = require('chai').expect;
  var chartit = require('./index'); 
+ var config={
+                grid:{
+                    padding:{
+                        left:5
+                    }
+                }
+            };
+var chart=chartit.init(config);
 
  describe('chartit', function(){
     it ('should work!',function(){
         expect(true).to.be.true;
+    });
+    describe('init', function(){
+        var chart=chartit.init(config);
+        it ('should merge the config Objects',function(){
+            expect(chart.config).to.contain.all.keys({'grid':{'padding':['top','right','bottom','left']}});
+        });
     });
  });
